7359667593cdde9d1ac1dcb60dff59eaf3996b68
avni90verma <75114137+avni90verma@users.noreply.github.com>
2024-12-27 03:35:26 +0530

    Tweetsy App completed

--- app/src/main/java/com/example/tweetsyapp/MainActivity.kt
+++ app/src/main/java/com/example/tweetsyapp/MainActivity.kt
@@ -5,13 +5,25 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.tweetsyapp.api.TweetsyApi
 import com.example.tweetsyapp.screens.CategoryScreen
 import com.example.tweetsyapp.screens.DetailsScreen
@@ -27,15 +39,51 @@ class MainActivity : ComponentActivity() {
 
     @Inject
     lateinit var tweetsyApi: TweetsyApi
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        GlobalScope.launch {
-            var response = tweetsyApi.getCategories()
-            Log.d("AVNI",response.body().toString())
-        }
         enableEdgeToEdge()
         setContent {
-          DetailsScreen()
+            TweetsyAppTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "Tweetsy") },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Color.Black,
+                                titleContentColor = Color.White
+                            )
+                        )
+                    }
+                ) {
+                    Box(modifier = Modifier.padding(it)) {
+                        App()
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "category") {
+        composable(route = "category")
+        {
+            CategoryScreen {
+                navController.navigate("detail/${it}")
+            }
+        }
+        composable(route = "detail/{category}",
+            arguments = listOf(
+                navArgument("category") {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            DetailsScreen()
         }
     }
 }
--- app/src/main/java/com/example/tweetsyapp/viewmodels/DetailViewModel.kt
+++ app/src/main/java/com/example/tweetsyapp/viewmodels/DetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.tweetsyapp.viewmodels
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tweetsyapp.models.TweetListItem
@@ -10,13 +11,16 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailViewModel @Inject constructor(private val repository: TweetRepository) : ViewModel() {
+class DetailViewModel @Inject constructor
+    (private val repository: TweetRepository,
+            private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     val tweets: StateFlow<List<TweetListItem>> = repository.tweets
 
     init {
         viewModelScope.launch {
-            repository.getTweets("android")
+            val category = savedStateHandle.get<String>("category")?:"motivation"
+            repository.getTweets(category)
         }
     }
 }
